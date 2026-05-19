da699648bfd8ef49c744cfc1c9bd6a64f448ca18
andikasihombing08-hub <andikasihombing08@gmail.com>
2026-05-19 14:23:52 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/member1')
+def member1():
+    return render_template('member1.html')
+
+@app.route('/member2')
+def member2():
+    return render_template('member2.html')
+
+@app.route('/member3')
+def member3():
+    return render_template('member3.html')
+
+@app.route('/member4')
+def member4():
+    return render_template('member4.html')
+
+@app.route('/member5')
+def member5():
+    return render_template('member5.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
